b5beae09ff3b71f478a0b529895955e039851e93
vsDeus <camioane112@gmail.com>
2020-01-15 22:12:56 +0200

    More detailes in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as fh:
+    readme_description = fh.read()
+
 setup(
     name             = 'giphyfun',
-    version          = '0.1',
+    version          = '0.2',
     description      = 'Command line program that play random giphy.',
     url              = 'https://github.com/vsDeus/giphyFun',
     download_url     = 'https://github.com/vsDeus/giphyFun/archive/0.1.tar.gz',
     author           = 'Goia Ciprian',
     author_email     = 'goia.ciprian14@gamil.com',
+    long_description =  readme_description,
+    long_description_content_type = "text/markdown",
     keywords         = ['giphy', 'terminal', 'giphyfun'],
     packages         = find_packages(),
     install_requires = ['opencv-python', 'appdirs', 'clipboard'],
@@ -15,12 +20,11 @@ setup(
         'console_scripts': [
             'giphyfun=giphyFun.giphyFun:main',
         ],
-    }
+    },
     classifiers      = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU GPLv3 License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
